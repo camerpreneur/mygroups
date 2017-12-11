c73a98c4e4d7702aa5d8f9f52a88c704c23e9d6b
kana patrick <foka1kana@gmail.com>
2017-12-11 20:09:07 +0100

    Update mygroups.php
    custum object

--- views/default/object/mygroups.php
+++ views/default/object/mygroups.php
@@ -1,29 +1,29 @@
 <?php
 
 $full = elgg_extract("full_view", $vars, FALSE);
+$mygroups = elgg_extract('entity', $vars, FALSE);
 
-
-if($full_view){
-    echo elgg_view('output/plaintext', ['value' => $vars['entity']->description]);
-    echo elgg_view('output/dropdown', ['text' => $vars['entity']->progress]);
-    echo elgg_view('output/dropdown', ['text' => $vars['entity']->sectorindustry]);
-    echo elgg_view('output/dropdown', ['text' => $vars['entity']->activity]);
-    echo elgg_view('output/dropdown', ['text' => $vars['entity']->markettype]);
-    echo elgg_view('output/dropdown', ['text' => $vars['entity']->typemark]);
-    echo elgg_view('output/dropdown', ['text' => $vars['entity']->offertype]);
-    echo elgg_view('output/dropdown', ['text' => $vars['entity']->turnover]);
-    echo elgg_view('output/dropdown', ['text' => $vars['entity']->currency]);
-    echo elgg_view('output/tags', ['tags' => $vars['entity']->location]);
-    echo elgg_view('output/url', ['href' => $vars['entity']->projectwebsite]);
-    echo elgg_view('output/url', ['href' => $vars['entity']->projectblog]);
-    echo elgg_view('output/url', ['href' => $vars['entity']->projectpitch]);
+if($full){
+    echo elgg_view('output/plaintext', ['value' => $mygroups->description]);
+    echo elgg_view('output/dropdown', ['text' => $mygroups->progress]);
+    echo elgg_view('output/dropdown', ['text' => $mygroups->sectorindustry]);
+    echo elgg_view('output/dropdown', ['text' => $mygroups->activity]);
+    echo elgg_view('output/dropdown', ['text' => $mygroups->markettype]);
+    echo elgg_view('output/dropdown', ['text' => $mygroups->typemark]);
+    echo elgg_view('output/dropdown', ['text' => $mygroups->offertype]);
+    echo elgg_view('output/dropdown', ['text' => $mygroups->turnover]);
+    echo elgg_view('output/dropdown', ['text' => $mygroups->currency]);
+    echo elgg_view('output/tags', ['tags' => $mygroups->location]);
+    echo elgg_view('output/url', ['href' => $mygroups->projectwebsite]);
+    echo elgg_view('output/url', ['href' => $mygroups->projectblog]);
+    echo elgg_view('output/url', ['href' => $mygroups->projectpitch]);
       
 }else {
     
     echo elgg_view_title(
         elgg_view('output/url', array(
-            'href' => $vars['entity']->getURL(),
-            'text' => $vars['entity']->name,
+            'href' => $mygroups->getURL(),
+            'text' => $mygroups->name,
             'is_trusted' => true
     )));  
 }
